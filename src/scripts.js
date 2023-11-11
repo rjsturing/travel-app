6d9681c9c32d2a9eca3c261b140ef036612591e3
Bobby Steckline <rjsturing@gmail.com>
2023-11-11 15:28:54 -0500

    Have landing page created

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,8 @@
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
 
+import './images/trips-icon.png'
+import './images/user-icon.png'
+import './images/main-logo.png'
+
 console.log('This is the JavaScript entry file - your code begins here.');
